403d02635896e62e9f241e5e8f50a0ea68cfa0b0
vegalozanolaura <106939179+vegalozanolaura@users.noreply.github.com>
2022-07-22 17:35:59 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,10 @@
+document.getElementById("button-up").addEventListener("click", scrollUp);
+function scrollUp(){
+    var currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 10));
+    }
+}
+buttonUp = document.getElementById("button-up");
